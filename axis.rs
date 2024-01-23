94765e7a0e6343609603db8e5ece5ef18d8b3457
EthanThomps <81367830+EthanThomps@users.noreply.github.com>
2024-01-23 13:57:18 -0500

    Delete axis.rs

--- axis.rs
+++
@@ -1,8 +0,0 @@
-use crate::events::SupportedPlanets;
-
-pub fn find_semimajor() {}
-pub fn find_semiminor() {}
-
-/// prints out (f64, f64) as semi major and semi minor
-#[allow(unused)]
-pub fn planet_semimm(p: SupportedPlanets) {}
